0640a727a8f3831da2285adc97d33ec133c9f63d
Abdelhafidhbensalem <bensalemabdelhafidh@gmail.com>
2021-08-05 11:57:43 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { Switch, Route, Link } from 'react-router-dom';
+import Signup from './components/signup/Signup';
+import Signin from './components/signin/Signin';
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WORKSHOP AUTH</h1>
+      <Switch>
+        <Route exact path="/"><Link to="/login"><button>Signin</button></Link><Link to="/register"><button>Signup</button></Link></Route>
+        <Route path="/register" component={Signup} />
+        <Route path="/login" component={Signin} />
+        {/*<Route path="/" />*/}
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        { /* <PrivateRoute path="/profil" component={Profil}/>*/}
+
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
